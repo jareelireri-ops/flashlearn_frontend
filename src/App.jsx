e27ea5dd56d3f45f75a89515db1fc1ab5d72e029
jareelireri-ops <jareel.ireri@student.moringaschool.com>
2026-06-25 10:36:30 +0300

    React setup,with folders and tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css'
 function App() {
   return (
     <div>
-      <h1>FlashLearn Frontend is Live!</h1>
+      <h1 className="text-3xl font-bold text-red-500">FlashLearn Frontend is Live!</h1>
     </div>
   )
 }
