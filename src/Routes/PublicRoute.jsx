2ad881b9ded527fba1b007752a79883652e38013
jareelireri-ops <jareel.ireri@student.moringaschool.com>
2026-07-29 04:29:10 +0300

    Update error redirect

--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -1,16 +1,11 @@
-import { Navigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
 
 function PublicRoute({ children }) {
-  const { user, loading } = useContext(AuthContext)
+  const { loading } = useContext(AuthContext)
 
   if (loading) {
     return <div>Loading...</div>
-  }
-
-  if (user) {
-    return <Navigate to={user.role === 'admin' ? '/admin' : '/dashboard'} replace />
   }
 
   return children
